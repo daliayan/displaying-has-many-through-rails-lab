0c0a48d8d6863a65ab58af1154913e5272b6d74a
daliayan <daliayan5@gmail.com>
2021-05-27 10:01:30 -0400

    Need to work on DR/Patient controllers + views

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,21 @@
-class DoctorsController < ApplicationController
+  class DoctorsController < ApplicationController
 
+    before_action :set_doctor, only: [:show]
+  
+    def index
+      @doctors = Doctor.all
+    end
+  
     def show
     end
-
-    def index
-        @doctors = Doctor.all
+  
+    
+    def set_doctor
+      @doctor = Doctor.find(params[:id])
     end
 
-end
+    def doctor_params
+      params.require(:doctor).permit(:name, :department)
+    end
+
+  end
